aa010daccc5f5b1b2ab4e6695e3a7b108dbe0a2c
fortaleza <jakub.zarembinski@yahoo.com>
2015-03-25 08:55:53 +0100

    v3.4

--- BitSharesHashGUI/src/main/java/sx/neura/bts/Version.java
+++ BitSharesHashGUI/src/main/java/sx/neura/bts/Version.java
@@ -1,6 +1,6 @@
 package sx.neura.bts;
 
 public class Version {
-	public static final String id = "0.3.3k";
+	public static final String id = "0.3.4";
 	public static final String name = "downton";
 }
